143897203e73ff2405e7d6db24ccc013cc46d9a6
Sunetra1998 <73545503+Sunetra1998@users.noreply.github.com>
2021-04-10 00:25:07 +0530

    Delete Contentful.js

--- Contentful.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "contentful";
-
-//const contentful = require("contentful");
-
-export default createClient({
-  space: REACT_APP_API_SPACE,
-  REACT_APP_ACCESS_TOKEN:
-    "61dda3b65d92ab91decdcde3f89f5d268746742a792e27b9981a33a1a76c8c7f",
-});
