62863c04424c51d5319bdb352b5db0e3f24256f7
Sunetra1998 <73545503+Sunetra1998@users.noreply.github.com>
2021-04-10 15:22:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,19 +6,65 @@ import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 import Navbar from "./components/Navbar";
 import { Route, Switch } from "react-router";
+import { Component } from "react";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms/" component={Rooms} />
-        <Route exact path="/rooms/:slug" component={SingleRoom} />
-        <Route component={Error} />
-      </Switch>
-    </>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyBSawNS1hKhRJFppzavRFxyG4ruWWRvyY4",
+  authDomain: "easyway-c337c.firebaseapp.com",
+});
+
+class App extends Component {
+  state = { isSignedIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/rooms/" component={Rooms} />
+                <Route exact path="/rooms/:slug" component={SingleRoom} />
+                <Route component={Error} />
+              </Switch>
+            </div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
